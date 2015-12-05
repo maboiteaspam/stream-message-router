72ca718d7e5eeaee6a1803753545c629d800d2fe
clement <maboiteaspam@wanadoo.fr>
2015-12-05 18:51:23 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var streamMsger = require('stream-messenger')
+var debug = require('debug')('message-router')
+
+module.exports = function messageRouter (message) {
+  var fnTransform = function (chunk, enc, cb) {
+    if (chunk.message && chunk.message===message) {
+      debug('%s: push message=%s', message, chunk.message)
+      this.push(chunk)
+    }
+    cb(null)
+  };
+  return streamMsger('router', fnTransform);
+};
---
+++ playground.js
@@ -0,0 +1,33 @@
+
+var flower = require('flower')
+var messageRouter = require('./index')
+
+var streamA = flower();
+
+var streamB = messageRouter('hello');       // stream-message-route is a stream which push only
+var streamC = messageRouter('goodbye');     // the messages{message: type} matching given value.
+
+streamA.pipe(streamB);                      // streamA will pipe many kind of messages,
+streamA.pipe(streamC);                      // but streamB and streamC routes only specific
+                                            // message types.
+streamB.on('data', function (d) {
+  console.log('streamB %j', d);
+})
+
+streamC.on('data', function (d) {
+  console.log('streamC %j', d);
+})
+
+streamA.on('data', function (d) {
+  console.log('streamA %j', d);
+})
+
+streamA.write({                             // write some messages Object {
+  message : 'hello',                        //    type: the message type
+  body    : 'the world'                     //    body: the message body
+});                                         // }
+streamA.write({
+  message : 'goodbye',                      // another kind of message.
+  body    : 'the world'
+});
+
