0f0d38b8fc398075fec705ae9160f2d2399eae38
clement <maboiteaspam@wanadoo.fr>
2015-12-06 11:27:34 +0800

    better debug output

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 var streamMsger = require('stream-messenger')
 var debug = require('debug')('message-router')
 
-module.exports = function messageRouter (message) {
+module.exports = function messageRouter (message, name) {
   var fnTransform = function (chunk, enc, cb) {
     if (chunk.message && chunk.message===message) {
-      debug('%s: push message=%s', message, chunk.message)
+      debug('%s is pushing a %s message',
+        (name || message + 'Listener'),
+        message);
       this.push(chunk)
     }
     cb(null)
   };
-  return streamMsger('router', fnTransform);
+  return streamMsger(name || message, fnTransform);
 };
